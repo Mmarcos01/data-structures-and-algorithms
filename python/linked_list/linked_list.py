6e073ff4cb8b253ca257f5427ece1efd1e0b9096
Marie <marie.marcos25@gmail.com>
2021-06-20 21:14:18 -0700

    link list includes

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,32 @@
+class Node:
+
+    def __init__(self, value, next = None):
+        self.value = value
+        self.next = next
+
 class LinkedList:
     """
     Put docstring here
     """
 
-    def __init__(self):
+    def __init__(self, head = None):
+        self.head = head
         # initialization here
-        pass
+        # pass
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, any_value):
+        node = Node(any_value)
+
+        if self.head is not None:
+            node.next == self.head
+        self.head == node
+
+    def includes(self, value):
+
+        current_value = self.head
+
+        while current_value != None:
+            if current_value.head == value:
+                return True
+            current_value = current_value.next
+        return False
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,27 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
 
 
 def test_import():
     assert LinkedList
+
+
+def test_empty_linked_list():
+    empty = LinkedList()
+    assert empty
+
+def test_instantiate_node():
+    node = Node('green', None)
+    actual = node.value
+    expected = 'green'
+    assert actual == expected
+    assert node.next == None
+
+def test_insert():
+    node = Node('blue', None)
+    insert_node = Node('red', node)
+    actual = insert_node.next.value
+    expected = 'blue'
+    assert actual == expected
+
+# def test_includes():
+#     linked_list = LinkedList(Node())
