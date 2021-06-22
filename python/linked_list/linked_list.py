ec9a7c085715134a5aca7c3a215e9a7daf94cd19
Marie <marie.marcos25@gmail.com>
2021-06-21 18:15:31 -0700

    tests to string and includes

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -6,27 +6,31 @@ class Node:
 
 class LinkedList:
     """
-    Put docstring here
+    create a linked list, ability to insert new nodes and check for existing nodes
     """
 
     def __init__(self, head = None):
         self.head = head
-        # initialization here
-        # pass
+
+    def __str__(self):
+        output = ''
+        current_value = self.head
+        while current_value:
+            output += f'{ {current_value.value} } -> '
+            #walk/traverse
+            current_value = current_value.next
+        output += 'None'
+        return output
 
     def insert(self, any_value):
-        node = Node(any_value)
-
-        if self.head is not None:
-            node.next == self.head
-        self.head == node
+        self.head = Node(any_value, self.head)
 
     def includes(self, value):
 
         current_value = self.head
 
-        while current_value != None:
-            if current_value.head == value:
+        while current_value:
+            if current_value.value == value:
                 return True
             current_value = current_value.next
         return False
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,9 +1,7 @@
 from linked_list.linked_list import LinkedList, Node
 
-
 def test_import():
     assert LinkedList
-
 
 def test_empty_linked_list():
     empty = LinkedList()
@@ -16,12 +14,27 @@ def test_instantiate_node():
     assert actual == expected
     assert node.next == None
 
-def test_insert():
-    node = Node('blue', None)
-    insert_node = Node('red', node)
-    actual = insert_node.next.value
-    expected = 'blue'
+# def test_insert():
+#     node = Node('blue', None)
+#     insert_node = Node('red', node)
+#     actual = insert_node.next.value
+#     expected = 'blue'
+#     assert actual == expected
+
+def test_includes_and_inserts():
+    ll1 = LinkedList()
+    ll1.insert('red')
+    ll1.insert('blue')
+    ll1.insert('green')
+    actual =  ll1.includes('red')
+    expected = True
     assert actual == expected
 
-# def test_includes():
-#     linked_list = LinkedList(Node())
+def test_to_string():
+    ll1 = LinkedList()
+    ll1.insert('red')
+    ll1.insert('blue')
+    ll1.insert('green')
+    actual = ll1.__str__()
+    expected = "{'green'} -> {'blue'} -> {'red'} -> None"
+    assert actual == expected
