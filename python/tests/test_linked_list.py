d0b8e09c3cf4eb71ed9fa334c8e40f73863a085e
Marie <marie.marcos25@gmail.com>
2021-07-06 20:40:01 -0700

    stacks and queues tests good

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,18 +1,18 @@
 from linked_list.linked_list import LinkedList, Node, zipLists
 
-def test_import():
-    assert LinkedList
+# def test_import():
+#     assert LinkedList
 
-def test_empty_linked_list():
-    empty = LinkedList()
-    assert empty
+# def test_empty_linked_list():
+#     empty = LinkedList()
+#     assert empty
 
-def test_instantiate_node():
-    node = Node('green', None)
-    actual = node.value
-    expected = 'green'
-    assert actual == expected
-    assert node.next == None
+# def test_instantiate_node():
+#     node = Node('green', None)
+#     actual = node.value
+#     expected = 'green'
+#     assert actual == expected
+#     assert node.next == None
 
 # def test_insert():
 #     node = Node('blue', None)
@@ -21,89 +21,89 @@ def test_instantiate_node():
 #     expected = 'blue'
 #     assert actual == expected
 
-def test_includes_and_inserts():
-    ll1 = LinkedList()
-    ll1.insert('red')
-    ll1.insert('blue')
-    ll1.insert('green')
-    actual =  ll1.includes('red')
-    expected = True
-    assert actual == expected
+# def test_includes_and_inserts():
+#     ll1 = LinkedList()
+#     ll1.insert('red')
+#     ll1.insert('blue')
+#     ll1.insert('green')
+#     actual =  ll1.includes('red')
+#     expected = True
+#     assert actual == expected
 
-def test_to_string():
-    ll1 = LinkedList()
-    ll1.insert('red')
-    ll1.insert('blue')
-    ll1.insert('green')
-    actual = ll1.__str__()
-    expected = "{'green'} -> {'blue'} -> {'red'} -> None"
-    assert actual == expected
+# def test_to_string():
+#     ll1 = LinkedList()
+#     ll1.insert('red')
+#     ll1.insert('blue')
+#     ll1.insert('green')
+#     actual = ll1.__str__()
+#     expected = "{'green'} -> {'blue'} -> {'red'} -> None"
+#     assert actual == expected
 
-def test_append():
-    my_list = LinkedList(Node('green', Node('blue', Node ('red'))))
-    my_list.append('orange')
-    actual = my_list.__str__()
-    expected = "{'green'} -> {'blue'} -> {'red'} -> {'orange'} -> None"
-    assert actual == expected
+# def test_append():
+#     my_list = LinkedList(Node('green', Node('blue', Node ('red'))))
+#     my_list.append('orange')
+#     actual = my_list.__str__()
+#     expected = "{'green'} -> {'blue'} -> {'red'} -> {'orange'} -> None"
+#     assert actual == expected
 
-def test_insertAfter():
-    my_list = LinkedList(Node('green', Node('blue', Node ('red'))))
-    my_list.insertAfter('blue', 'orange')
-    actual = my_list.__str__()
-    expected = "{'green'} -> {'blue'} -> {'orange'} -> {'red'} -> None"
-    assert actual == expected
+# def test_insertAfter():
+#     my_list = LinkedList(Node('green', Node('blue', Node ('red'))))
+#     my_list.insertAfter('blue', 'orange')
+#     actual = my_list.__str__()
+#     expected = "{'green'} -> {'blue'} -> {'orange'} -> {'red'} -> None"
+#     assert actual == expected
 
-def test_insertBefore():
-    my_list = LinkedList(Node('green', Node('blue', Node ('red'))))
-    my_list.insertBefore('blue', 'orange')
-    actual = my_list.__str__()
-    expected = "{'green'} -> {'orange'} -> {'blue'} -> {'red'} -> None"
-    assert actual == expected
+# def test_insertBefore():
+#     my_list = LinkedList(Node('green', Node('blue', Node ('red'))))
+#     my_list.insertBefore('blue', 'orange')
+#     actual = my_list.__str__()
+#     expected = "{'green'} -> {'orange'} -> {'blue'} -> {'red'} -> None"
+#     assert actual == expected
 
-def test_empty_linked_list():
-    empty = LinkedList()
-    assert empty
+# def test_empty_linked_list():
+#     empty = LinkedList()
+#     assert empty
 
-def test_k_is_greater_than_ll_length():
-    ll1 = LinkedList()
-    ll1.append("a").append("b").append("c").append("d").append("e")
-    actual = ll1.kth_from_the_end(2)
-    expected = "c"
-    assert actual == expected
+# def test_k_is_greater_than_ll_length():
+#     ll1 = LinkedList()
+#     ll1.append("a").append("b").append("c").append("d").append("e")
+#     actual = ll1.kth_from_the_end(2)
+#     expected = "c"
+#     assert actual == expected
 
-def test_k_and_the_length_of_the_list_are_the_same():
-    ll1 = LinkedList()
-    ll1.append("a").append("b").append("c").append("d").append("e")
-    actual = ll1.kth_from_the_end(5)
-    expected = "a"
-    assert actual == expected
+# def test_k_and_the_length_of_the_list_are_the_same():
+#     ll1 = LinkedList()
+#     ll1.append("a").append("b").append("c").append("d").append("e")
+#     actual = ll1.kth_from_the_end(5)
+#     expected = "a"
+#     assert actual == expected
 
-def test_k_is_negative():
-    ll1 = LinkedList()
-    ll1.append("a").append("b").append("c").append("d").append("e")
-    actual = ll1.kth_from_the_end(-5)
-    expected = "K is negative"
-    assert actual == expected
+# def test_k_is_negative():
+#     ll1 = LinkedList()
+#     ll1.append("a").append("b").append("c").append("d").append("e")
+#     actual = ll1.kth_from_the_end(-5)
+#     expected = "K is negative"
+#     assert actual == expected
 
-def test_linked_list_is_of_a_size_1():
-    ll1 = LinkedList()
-    ll1.append("a")
-    actual = ll1.kth_from_the_end(1)
-    expected = "a"
-    assert actual == expected
+# def test_linked_list_is_of_a_size_1():
+#     ll1 = LinkedList()
+#     ll1.append("a")
+#     actual = ll1.kth_from_the_end(1)
+#     expected = "a"
+#     assert actual == expected
 
-def test_k_is_somewhere_in_the_middle_of_the_linked_list():
-    ll1 = LinkedList()
-    ll1.append("a").append("b").append("c").append("d").append("e")
-    actual = ll1.kth_from_the_end(3)
-    expected = "b"
-    assert actual == expected
+# def test_k_is_somewhere_in_the_middle_of_the_linked_list():
+#     ll1 = LinkedList()
+#     ll1.append("a").append("b").append("c").append("d").append("e")
+#     actual = ll1.kth_from_the_end(3)
+#     expected = "b"
+#     assert actual == expected
 
-def test_zip_two_lists():
-    ll1 = LinkedList()
-    ll2 = LinkedList()
-    ll1.append("a").append("c").append("e")
-    ll2.append("b").append("d").append("f")
-    actual = zipLists(ll1,ll2)
-    expected = "{'a'} -> {'b'} -> {'c'} -> {'d'} -> {'e'} -> {'f'} -> None"
-    assert str(actual) == expected
+# def test_zip_two_lists():
+#     ll1 = LinkedList()
+#     ll2 = LinkedList()
+#     ll1.append("a").append("c").append("e")
+#     ll2.append("b").append("d").append("f")
+#     actual = zipLists(ll1,ll2)
+#     expected = "{'a'} -> {'b'} -> {'c'} -> {'d'} -> {'e'} -> {'f'} -> None"
+#     assert str(actual) == expected
--- python/tests/test_stacks_queues.py
+++ python/tests/test_stacks_queues.py
@@ -1,22 +1,65 @@
-from stacks_queues.stacks_queues import Node, Stack
+from stacks_queues.stacks_queues import Node, Stack, Queue
 
-def test_isEmpty():
+def test_stack_isEmpty():
     my_stack = Stack()
-    actual = my_stack.__str__()
-    expected = "None"
+    actual = my_stack.isEmpty()
+    expected = True
     assert actual == expected
 
-def test_push():
-    my_stack = Stack(Node('a', Node('b', Node ('c'))))
-    my_stack.push('d')
-    my_stack.push('e')
+def test_stack_push_multiple():
+    my_stack = Stack()
+    my_stack.push('a')
+    my_stack.push('b')
     actual = my_stack.__str__()
-    expected = "{'a'} -> {'b'} -> {'c'} -> {'d'} -> {'e'} -> None"
+    expected = "{'b'} -> {'a'} -> None"
     assert actual == expected
 
-def test_pop():
-    my_stack = Stack(Node('a', Node('b', Node ('c'))))
+def test_stack_pop():
+    my_stack = Stack()
+    my_stack.push('a')
+    my_stack.push('b')
+    my_stack.push('c')
     my_stack.pop()
-    actual = my_stack.__str__()
-    expected = "{'a'} -> {'b'} -> None"
+    actual = my_stack.top.value
+    expected = 'b'
+    assert actual == expected
+
+def test_stack_peek():
+    my_stack = Stack()
+    my_stack.push('test')
+    my_stack.push('new')
+    actual = my_stack.peek()
+    expected = 'new'
+    assert actual == expected
+
+def test_queue_isEmpty():
+    my_queue = Queue()
+    actual = my_queue.isEmpty()
+    expected = True
+    assert actual == expected
+
+def test_enqueue():
+    my_queue = Queue()
+    my_queue.enqueue('a')
+    my_queue.enqueue('b')
+    my_queue.enqueue('c')
+    actual = my_queue.rear.value
+    expected = 'c'
+    assert actual == expected
+
+def test_dequeue():
+    my_queue = Queue()
+    my_queue.enqueue('a')
+    my_queue.enqueue('b')
+    my_queue.enqueue('c')
+    actual = my_queue.rear.value
+    expected = 'c'
+    assert actual == expected
+
+def test_queue_peek():
+    my_queue = Queue()
+    my_queue.enqueue('a')
+    my_queue.enqueue('b')
+    actual = my_queue.peek()
+    expected = 'a'
     assert actual == expected
